66240f217ee065cca9e1e3e81d8b5c505bc884f6
actiangent <hisyam.fausta@gmail.com>
2022-10-03 20:51:31 +0700

    quiz-pertemuan-3 fifth commit

--- quiz-pertemuan-3/quiz-4/README.md
+++ quiz-pertemuan-3/quiz-4/README.md
@@ -1,3 +1,3 @@
-Make a matrix 2X2 with python
+Make a matrix 2X2 and sum it up with python
 
-![screenshot](https://raw.githubusercontent.com/actiangent/bahasa-pemrograman/main/quiz-pertemuan-3/images/Screenshot%202022-10-03%20203410.jpg)
+![screenshot](https://raw.githubusercontent.com/actiangent/bahasa-pemrograman/main/quiz-pertemuan-3/images/Screenshot%202022-10-03%20204258.jpg)
--- quiz-pertemuan-3/quiz-4/quiz-4.py
+++ quiz-pertemuan-3/quiz-4/quiz-4.py
@@ -1,4 +1,14 @@
-matrix = [[17, 21], [31, 19]]
+matrix_1 = [[17, 21], [31, 19]]
+matrix_2 = [[51, 15], [84, 96]]
 
-for i in matrix:
+def add_matrices(first_matrix, second_matrix):
+    result = []
+    for i in range(len(first_matrix)):
+        row = []
+        for j in range(len(first_matrix[0])):
+            row.append(first_matrix[i][j]+second_matrix[i][j])
+        result.append(row)
+    return result
+
+for i in add_matrices(matrix_1, matrix_2):
     print(i)
